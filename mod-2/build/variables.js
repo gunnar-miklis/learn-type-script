796184505b972fc8dc2313ff13eec95eb5e1cd7f
gunnar-miklis <gunnar.miklis@gmail.com>
2023-06-12 20:46:56 +0200

    Update

--- mod-2/build/variables.js
+++ mod-2/build/variables.js
@@ -1,23 +1,17 @@
+"use strict";
 // dynamic run-time type system (JS) vs static compile-time type styem (TS)
 //	* static enable to accurately express the type relationships that are exprected
 //	* pre-validation while compiling
 //	* provide better documentation for the intention of the code
-
-let x: number; 	// TS: explicit declaration
-let y = 1; 		// JS: implicit declaration
-let z;			// JS: without initialization
-
+let x; // TS: explicit declaration
+let y = 1; // JS: implicit declaration
+let z; // JS: without initialization
 // initialize, obviously only possible with numbers
 x = 1;
-
 // intellisense only suggests number-related methods
-console.log( y.toFixed(2) );
-
+console.log(y.toFixed(2));
 // type: any
-z = 'bcd'
-
-
-
+z = 'bcd';
 // COMMENT: "Type safety" is one of the main motivations for using TypeScript.
 // types:
 //	* any: any value without constraints
@@ -25,58 +19,51 @@ z = 'bcd'
 //	* objects: array, class, interface, literals
 //	* parameters
 //	* null / undefined
-
 // NOTE: primitives
 // boolean
-const yesNo: boolean = false;
+const yesNo = false;
 // number
-const num: number = 0;
+const num = 0;
 // big numbers
-const bigNum: bigint = 100n;
+const bigNum = 100n;
 // strings
-const str: string = 'Hello World!';
-
+const str = 'Hello World!';
 // enum
 //	* a symbolic name for a set of values
 //	* used to create sets of constants
 //	* makes code easier to read
 //	By default, enum values begin with a value of 0, so Permanent is 0, Temp is 1, and Apprentice is 2
-enum ContractStatus {
-	Permanent = 1,
-	Temp,
-	Apprentice
-}
+var ContractStatus;
+(function (ContractStatus) {
+    ContractStatus[ContractStatus["Permanent"] = 1] = "Permanent";
+    ContractStatus[ContractStatus["Temp"] = 2] = "Temp";
+    ContractStatus[ContractStatus["Apprentice"] = 3] = "Apprentice";
+})(ContractStatus || (ContractStatus = {}));
 // declare a new variable with the type "ContractStatus"
-let employeeStatus: ContractStatus;
+let employeeStatus;
 // refer to a enum value ( like an object )
-employeeStatus = ContractStatus.Temp
+employeeStatus = ContractStatus.Temp;
 // display the value
-console.log( employeeStatus ); // :>> 2
+console.log(employeeStatus); // :>> 2
 // display the name
-console.log( ContractStatus[employeeStatus]); // :>> Temp
-
-
+console.log(ContractStatus[employeeStatus]); // :>> Temp
 // NOTE: type any: bypass any compile-time checks = flexible at the cost of losing type safety
-let anyValue: any;
-anyValue = 10;		// Ok
-anyValue = 'abc';	// Ok
-anyValue = true;		// Ok
+let anyValue;
+anyValue = 10; // Ok
+anyValue = 'abc'; // Ok
+anyValue = true; // Ok
 // NOTE: type unkown: unable to interact with a variable of type unkown
-let unkownValue: unknown;
+let unkownValue;
 unkownValue = 10;
 unkownValue = 'abc';
 unkownValue = true;
-
-
 // COMMENT: type assertion
 //	* A type assertion is like a type cast.
 //	* It tells the compiler "trust me, I know what I’m doing."
 //	* e.g. allows to force-apply a string method: toUpperCase().
 //	* best-practice: to make sure the type is the one we expect, we use "type guards"
 anyValue = 'hello';
-(anyValue as string).toUpperCase(); // :>> HELLO
-
-
+anyValue.toUpperCase(); // :>> HELLO
 // COMMENT: type guards
 //	* conditional test, to learn the type of a variable
 //	* "typeof" or "instanceof"
@@ -88,9 +75,9 @@ anyValue = 'hello';
 // undefined	|	typeof undefined === "undefined"
 // function		|	typeof f === "function"
 // array		|	Array.isArray(a)
-if ( typeof anyValue === 'string' ) {
-	console.log( (anyValue as string).toUpperCase() ); // :>> HELLO
+if (typeof anyValue === 'string') {
+    console.log(anyValue.toUpperCase()); // :>> HELLO
 }
 else {
-	console.log( 'Error - expected anyValue to be typeof String.');
+    console.log('Error - expected anyValue to be typeof String.');
 }
