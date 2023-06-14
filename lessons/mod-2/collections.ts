f745266365de64e2684c6cbd8798f4325f15bdd4
gunnar-miklis <gunnar.miklis@gmail.com>
2023-06-14 20:07:46 +0200

    Update

--- lessons/mod-2/collections.ts
+++ lessons/mod-2/collections.ts
@@ -5,6 +5,7 @@ let arr2: Array<number> = [ 1, 2, 3 ];
 
 // NOTE: tuples
 //	* arrays of mixed types
+//	* A Tuple is an array that has a specific number of elements. Each element is of a specific type and must appear in the exact same order when values are assigned to it.
 let person: [string, number];
 person = [ 'Mark', 30 ];			// Ok
 // person = [ 'Mark', 30, true ];	// invalid
