3a70e446b5a584ac310a2454ceeee8ac1fded980
gunnar-miklis <gunnar.miklis@gmail.com>
2023-06-14 22:18:18 +0200

    Update

--- lessons/mod-3/interfaces.ts
+++ lessons/mod-3/interfaces.ts
@@ -15,7 +15,7 @@ interface Employee {
 	fullName(): string;
 }
 
-// COMMENT: the ": string" is best practice since it specifies the "return type"
+// COMMENT: the ": string" is best-practice since it specifies the "return type" of the function
 const employee: Employee = {
 	firstName: 'Emil',
 	lastName: 'Anderson',
@@ -25,3 +25,50 @@ const employee: Employee = {
 }
 console.log( employee.firstName )
 console.log( employee.fullName() )
+
+
+
+// declare an interface
+interface PascalCaseName {
+	requiredProperty: string;
+	optionalProperty?: string;
+	readonly readOnlyProperty: number;
+}
+// implement an interface
+const obj: PascalCaseName = {
+	requiredProperty: 'required',
+	readOnlyProperty: 123456
+}
+console.log( obj.requiredProperty );
+console.log( obj.readOnlyProperty );
+
+// modify property values
+obj.requiredProperty = 'is required';
+// obj.readOnlyProperty = 654321 // invalid
+console.log( obj.requiredProperty );
+console.log( obj.readOnlyProperty );
+
+
+
+interface IceCream {
+	readonly flavor: string;
+	scoops: number;
+	instructions?: string;
+}
+const myIceCream: IceCream = {
+	flavor: 'chocolate',
+	scoops: 2
+}
+console.log( myIceCream.flavor );
+
+function tooManyScoops( dessert: IceCream ) {
+	return dessert.scoops > 3 ? dessert.scoops + ' is too much!' : 'here you go!'
+
+}
+console.log( tooManyScoops( myIceCream ));
+
+myIceCream.scoops = 4;
+console.log( tooManyScoops( myIceCream ));
+
+myIceCream.instructions = 'sprinkle some stuff on top'
+console.log( myIceCream );
