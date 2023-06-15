e4b1fdf7711e4657d8a626b479d0b26d4241e35b
gunnar-miklis <gunnar.miklis@gmail.com>
2023-06-15 17:11:46 +0200

    Update

--- lessons/mod-3/interfaces.ts
+++ lessons/mod-3/interfaces.ts
@@ -28,13 +28,13 @@ console.log( employee.fullName() )
 
 
 
-// declare an interface
+// NOTE: declare an interface
 interface PascalCaseName {
 	requiredProperty: string;
 	optionalProperty?: string;
 	readonly readOnlyProperty: number;
 }
-// implement an interface
+// NOTE: implement an interface
 const obj: PascalCaseName = {
 	requiredProperty: 'required',
 	readOnlyProperty: 123456
@@ -50,6 +50,7 @@ console.log( obj.readOnlyProperty );
 
 
 
+// example
 interface IceCream {
 	readonly flavor: string;
 	scoops: number;
@@ -59,16 +60,80 @@ const myIceCream: IceCream = {
 	flavor: 'chocolate',
 	scoops: 2
 }
-console.log( myIceCream.flavor );
+console.log(
+	myIceCream.flavor );
 
 function tooManyScoops( dessert: IceCream ) {
 	return dessert.scoops > 3 ? dessert.scoops + ' is too much!' : 'here you go!'
-
 }
-console.log( tooManyScoops( myIceCream ));
+console.log(
+	tooManyScoops( myIceCream ));
 
 myIceCream.scoops = 4;
-console.log( tooManyScoops( myIceCream ));
+console.log(
+	tooManyScoops( myIceCream ));
 
 myIceCream.instructions = 'sprinkle some stuff on top'
-console.log( myIceCream );
+console.log(
+	myIceCream );
+
+
+
+// NOTE: extend an interface
+interface Sundae extends IceCream {
+	sauce: 'chocolate' | 'caramel' | 'strawberry';
+	nuts?: boolean;
+	whippedCream?: boolean;
+	instructions?: string;
+}
+const myIceCream2: Sundae = {
+	flavor: 'vanilla',
+	scoops: 3,
+	sauce: 'caramel',
+	nuts: false,
+	whippedCream: true
+}
+function tooManyScoops2( dessert: Sundae ) {
+	return dessert.scoops > 3 ? dessert.scoops + ' is too much!' : 'here you go!'
+}
+console.log( 
+	tooManyScoops2( myIceCream2 ));
+
+
+
+// NOTE: indexable interface
+interface IceCreamArray {
+	[index: number]: string;
+}
+const flavors: IceCreamArray = ['chocolate', 'vanilla', 'strawberry'];
+console.log( flavors[0] );
+
+
+
+// NOTE: describing APIs
+//	*  The interface provides you (and other developers alike) with a clear understanding of what an API is expecting and what it will return.
+const apiURL = 'https://jsonplaceholder.typicode.com/posts';
+interface Post {
+	userId: number;
+	id: number;
+	title: string;
+	body: string;
+}
+async function fetchPosts( url: string ) {
+	const response = await fetch( url );
+	const apiData = await response.json();
+	return apiData as Post[];
+}
+async function showPosts() {
+	const posts = await fetchPosts( apiURL );
+	const post1 = posts[0];
+	console.log( post1.userId );
+	console.log( post1.id );
+	console.log( post1.title );
+	console.log( post1.body );
+}
+showPosts();
+
+// COMMENT: While earlier versions of ECMAScript, such as ES3, do not support async and await, 
+//	* the TypeScript compiler is able to generate compatible code to implement this feature. 
+//	* This enables you to take advantage of the newer feature while still being able to target older browsers!
