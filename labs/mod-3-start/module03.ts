be862837a3b333f644fd31dbceb9d9a3a93c9c99
gunnar-miklis <gunnar.miklis@gmail.com>
2023-06-15 17:34:15 +0200

    Done

---
+++ labs/mod-3-start/module03.ts
@@ -0,0 +1,36 @@
+/* Module 3: Implement interfaces in TypeScript
+   Lab Start  */
+
+/*  EXERCISE 1
+    TODO: Declare the Loan interface. */
+interface Loan {
+	principal: number,
+	interestRate: number
+}
+
+/*  TODO: Declare the ConventionalLoan interface. */
+interface ConventionalLoan extends Loan {
+	months: number
+}
+
+/*  TODO: Update the calculateInterestOnlyLoanPayment function. */
+
+function calculateInterestOnlyLoanPayment( loanTerms: Loan ): string {
+    const interest: number = loanTerms.interestRate / 1200;
+    const payment: number = loanTerms.principal * interest;
+    return 'The interest only loan payment is ' + payment.toFixed(2);
+}
+
+/*  TODO: Update the calculateConventionalLoanPayment function. */
+
+function calculateConventionalLoanPayment( loanTerms: ConventionalLoan ): string {
+    const interest: number = loanTerms.interestRate / 1200;
+    const payment: number = loanTerms.principal * interest / (1 - (Math.pow(1 / (1 + interest), loanTerms.months)));
+    return 'The conventional loan payment is ' + payment.toFixed(2);
+}
+
+const interestOnlyPayment = calculateInterestOnlyLoanPayment( { principal: 30000, interestRate: 5 } );
+const conventionalPayment = calculateConventionalLoanPayment( { principal: 30000, interestRate: 5, months: 180 } );
+
+console.log(interestOnlyPayment);     //* Returns "The interest only loan payment is 125.00" 
+console.log(conventionalPayment);     //* Returns "The conventional loan payment is 237.24" 
